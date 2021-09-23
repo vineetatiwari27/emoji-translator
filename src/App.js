f76668bc57855bbf5207629c8263cb4c05d3932b
vineetatiwari27 <85996556+vineetatiwari27@users.noreply.github.com>
2021-09-23 18:58:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojidictionary = {
+  "😊": "smilingEmoji",
+  "🐷": "pig face",
+  "🍇": "grapes",
+  "🍉": "water-melon",
+  "😎": "cool",
+  "😕": "confused face",
+  "😯": "hushed face"
+};
+var emojisWeKnow = Object.keys(emojidictionary);
+
+export default function App() {
+  var [emojiInput, emojiOutput] = useState("");
+  function textInput(event) {
+    var userInput = event.target.value;
+    var emojiInput = emojidictionary[userInput];
+    if (emojiInput === undefined) {
+      emojiInput = "can't recognize this :(";
+    }
+    emojiOutput(emojiInput);
+  }
+
+  function emojiClickHandler(emoji) {
+    var emojiInput = emojidictionary[emoji];
+    emojiOutput(emojiInput);
+  }
+
+  return (
+    <div className="App">
+      <h1>Emoji Translator</h1>
+      <input onChange={textInput}></input>
+
+      <div>{emojiInput}</div>
+      <h3>emojis we know :</h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            key={emoji}
+            style={{ fontSize: "3rem", cursor: "pointer" }}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
